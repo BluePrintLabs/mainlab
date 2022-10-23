dc9ecf58028a9b3b9d31d18cfaf85be52f3222bc
BluePrintLabs <100381115+BluePrintLabs@users.noreply.github.com>
2022-10-23 17:50:18 -0500

    Update index.js

--- socialexperiment/javaScript/index.js
+++ socialexperiment/javaScript/index.js
@@ -1,1 +1,15 @@
-Test
+callWs = function(){
+	// The Endpoint URL
+	let url = 'https://jsonplaceholder.typicode.com/posts/1';
+	fetch(url)
+  .then(function(response) {
+  	// Render the Response Status
+  	document.getElementById('result').innerHTML = response.status;
+    // Parse the body as JSON
+    return response.json();
+  })
+  .then(function(json) {
+  	// Render the parsed body
+  	document.getElementById('result_json').innerHTML = JSON.stringify(json);
+  })
+}
